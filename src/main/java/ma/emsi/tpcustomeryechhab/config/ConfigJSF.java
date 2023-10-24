a9b5ec239f2f9b36548d51f012172740ec812190
fz.yech <fatimayechhab@gmail.com>
2023-10-24 21:04:33 +0100

    Exécution du projet

---
+++ src/main/java/ma/emsi/tpcustomeryechhab/config/ConfigJSF.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package ma.emsi.tpcustomeryechhab.config;
+
+import jakarta.enterprise.context.ApplicationScoped;
+import jakarta.faces.annotation.FacesConfig;
+
+/**
+ *
+ * @author user
+ */
+@ApplicationScoped
+@FacesConfig
+public class ConfigJSF {
+}
---
+++ src/main/java/ma/emsi/tpcustomeryechhab/jsf/CustomerBean.java
@@ -0,0 +1,39 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/JSF/JSFManagedBean.java to edit this template
+ */
+package ma.emsi.tpcustomeryechhab.jsf;
+
+import jakarta.inject.Named;
+import jakarta.faces.view.ViewScoped;
+import jakarta.inject.Inject;
+import java.io.Serializable;
+import java.util.List;
+import ma.emsi.tpcustomeryechhab.entity.Customer;
+import ma.emsi.tpcustomeryechhab.service.CustomerManager;
+
+/**
+ *
+ * @author user
+ */
+@Named(value = "customerBean")  
+@ViewScoped  
+public class CustomerBean implements Serializable {  
+  private List<Customer> customerList;  
+
+  @Inject
+  private CustomerManager customerManager;  
+        
+  public CustomerBean() {  }  
+             
+  /** 
+   * Retourne la liste des clients pour affichage dans une DataTable.
+     * @return 
+   */  
+  public List<Customer> getCustomers() {
+    if (customerList == null) {
+      customerList = customerManager.getAllCustomers();
+    }
+    return customerList;
+  }  
+}
